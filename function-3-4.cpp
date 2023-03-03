f6f1c981657cada196439366378fcee9926c9574
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-03 14:33:03 +1030

    fixed 3-4

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -4,19 +4,19 @@ using namespace std;
 void print_pass_fail(char grade) {
     switch(grade) {
         case 'A':
-            cout << "pass";
+            cout << "pass" << endl;
             break;
         case 'B':
-            cout << "pass";
+            cout << "pass" << endl;
             break;
         case 'C':
-            cout << "pass";
+            cout << "pass" << endl;
             break;
         case 'D':
-            cout << "fail";
+            cout << "fail" << endl;
             break;
         case 'E':
-            cout << "fail";
+            cout << "fail" << endl;
             break;
     }
 }
