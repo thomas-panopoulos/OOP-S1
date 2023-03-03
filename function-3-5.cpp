a420143b03f481302867dfa1cb70378af2f99d8b
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-03 14:38:08 +1030

    3-5 fixed

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -9,5 +9,5 @@ double sum_even(double array[], int len) {
             sum += array[i];
         }
     }
-    cout << sum << endl;
+    return sum;
 }
--- main-3-5.cpp
+++ main-3-5.cpp
@@ -1,8 +1,9 @@
-
+#include <iostream>
+using namespace std;
 extern double sum_even(double[], int);
 
 int main() {
     double array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    sum_even(array, 10);
+    cout << sum_even(array, 10) << endl;
     return 0;
 }
