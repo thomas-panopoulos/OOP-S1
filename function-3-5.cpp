9397425f50dd83cf249f737a0eebb311f1359d23
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-03 14:44:56 +1030

    3-5 variable assignment fixed

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 double sum_even(double array[], int len) {
-    double sum = {0};
+    double sum{0};
     for (int i = 0; i < len; i++) {
         if (i % 2 == 0) {
             sum += array[i];
