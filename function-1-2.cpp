5859629c6bd5df1404af92ddf297e515d143f3fb
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-09 21:03:00 +1030

    1-2 changed

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,5 +1,12 @@
-
+#include <iostream>
+using namespace std;
 int is_identity(int array[10][10]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << array[i][j] << ' ';
+        }
+        cout << endl;
+    }
     for (int i = 0; i < 10; i++) {
         if (array[i][i] != 1) {
             return 0;
