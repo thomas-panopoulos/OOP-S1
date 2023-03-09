e67c0999206033d1520d5bfa2c0d0468fa12c03c
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-09 21:45:15 +1030

    1-2 FINALLY FIXED

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 int is_identity(int array[10][10]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout << array[i][j] << ' ';
-        }
-        cout << endl;
-    }
+
     for (int i = 0; i < 10; i++) {
         if (array[i][i] != 1) {
             return 0;
         }
-        for (int j = i+1, k = i-1; j < 10 && k >= 0; j++, k--) {
-            if (array[i][j] != 0 || array[i][k] != 0) {
+        for (int j = i+1; j < 10; j++) {
+            if (array[i][j] != 0) {
+                return 0;
+            }
+        }
+        for (int k = i-1; k > 0; k--) {
+            if (array[i][k] != 0) {
                 return 0;
             }
         }
