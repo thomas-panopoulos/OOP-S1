105597d46dce09d8bd4880fa3918ead4d4dd389b
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-09 21:51:22 +1030

    possible 1-3 seg fault fix

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,27 +1,21 @@
 #include <map>
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
 void count_digits(int array[4][4]) {
     map<int, int>values;
+    for (int i = 0; i < 9; i++) {
+        if (!values[i]) {
+            values[i] = 0;
+        }
+    }
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             if (array[i][j] < 10) {
-                if (!values[array[i][j]]) {
-                    values[array[i][j]] = 1;
-                }
-                else {
-                    values[array[i][j]] += 1;
-                }
+                values[array[i][j]] += 1;
             }
         }
-        for (int i = 0; i < 9; i++) {
-            if (!values[i]) {
-                values[i] = 0;
-            }
-        }
+
     }
-    
     cout << "0:" << values.at(0) << ";1:" << values.at(1) << ";2:" << values.at(2) << ";3:" << values.at(3) << ";4:" << values.at(4) << ";5:" << values.at(5) << ";6:" << values.at(6) << ";7:" << values.at(7) << ";8:" << values.at(8) << ";9:" << values.at(9) << ';' << endl;
 }
