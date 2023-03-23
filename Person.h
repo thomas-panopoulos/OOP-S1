4700ca9dcf2a799166e2f6e8163b1bb67f77b55b
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-03-23 20:57:18 +1030

    fixed person.h name

---
+++ Person.h
@@ -0,0 +1,20 @@
+#ifndef  PERSON_H
+#define PERSON_H
+
+#include <iostream>
+
+struct Person {
+    std::string name;
+    int age;
+};
+
+
+struct PersonList
+{
+    Person *people;
+    int numPeople;
+
+};
+
+
+#endif
