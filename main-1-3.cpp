440f8005266867cd6e4c7d8c4c93816bdd10ad4c
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-04-09 23:14:32 +0930

    fixed

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,3 +1,17 @@
+#include "Vehicle.h"
+#include "ParkingLot.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+#include <iostream>
+#include <string>
+
 int main() {
+    ParkingLot p(10);
+    for (int i = 0; i < 10; i++) {
+        Car c(i);
+        p.parkVehicle(&c);
+    }
+    p.countOverstayingVehicles(-1);
     return 0;
 }
