e49908742571ab771364821e3947bbc422089f2c
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-04-09 23:06:19 +0930

    fixed

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -5,7 +5,7 @@ Vehicle::Vehicle() : ID(0), timeOfEntry(0) {};
 
 Vehicle::Vehicle(int id) : ID(id), timeOfEntry(time(0)) {};
 
-int Vehicle::getParkingDuration(int id) {
+int Vehicle::getParkingDuration() {
     return time(NULL) - timeOfEntry;
 }
 int Vehicle::getID() { return ID;};
