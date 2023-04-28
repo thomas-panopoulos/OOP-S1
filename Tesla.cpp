44bc822838253e0c17ad279626233bb6479a22c3
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-04-28 14:14:42 +0930

    maybe fixed 2-1

--- Tesla.cpp
+++ Tesla.cpp
@@ -7,7 +7,7 @@ Tesla::Tesla(char model, int price) : Car(price), model(model) {};
 void Tesla::chargeBattery(int mins) {
     for (int i = 0; i < mins; i++) {
         if (batteryPercentage < 100) {
-            batteryPercentage += 1*0.5;
+            this->batteryPercentage += 1*0.5;
         }
     }
 };
@@ -15,9 +15,9 @@ void Tesla::chargeBattery(int mins) {
 void Tesla::drive(int kms) {
     for (int i = 0; i < kms; i++) {
         if (batteryPercentage > 0) {
-            emissions += 74*1;//possibly gradescope error
+            this->emissions += 74*1;//possibly gradescope error
             if (i % 5 == 0) {
-                batteryPercentage -= 1;
+                this->batteryPercentage -= 1;
             }
         }
     }
@@ -32,11 +32,11 @@ float Tesla::get_batteryPercentage() {
 }
 
 void Tesla::set_model(char model) {
-    model = model;
+    this->model = model;
 }
 
 void Tesla::set_batteryPercentage(float batteryPercentage) {
-    batteryPercentage = batteryPercentage;
+    this->batteryPercentage = batteryPercentage;
 }
 
 
