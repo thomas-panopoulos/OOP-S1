2c6760e527e2c372077ddd69da0410b37df31a2a
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2023-04-28 15:10:09 +0930

    maybe fix drive tesla

--- Tesla.cpp
+++ Tesla.cpp
@@ -18,20 +18,6 @@ void Tesla::drive(int kms) {
             this-> batteryPercentage = batteryPercentage - 0.2;
         }
     }
-/*
-    for (int i = 0; i < kms; i++) {
-        if (litresOfFuel > 0) {
-            emissions += 234*1;//possibly gradescope error
-            if (i % 5 == 0) {
-                litresOfFuel -= 1;
-            }
-        }
-    }
-        if (batteryPercentage > 0) {
-            this->emissions += 74*kms;//possibly gradescope error
-
-                this->batteryPercentage -= (float(kms)/5.0);
-        }*/
 }
 
 char Tesla::get_model() {
